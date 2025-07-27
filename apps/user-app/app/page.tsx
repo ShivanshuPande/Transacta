6ca489f2ab6a467a3a34291f73bea8c7c4589da1
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-07-27 22:23:36 +0530

    prisma as the orm

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,27 +1,6 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-import { Card } from "@repo/ui/card";
-
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
-
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen text-5xl font-bold">
       Hello there
     </div>
   );
