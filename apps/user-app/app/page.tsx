05e4370049ae06bff8b64be3276711a4a886894a
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-07-27 22:33:54 +0530

    exporting the db

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,3 +1,7 @@
+import {PrismaClient} from "@repo/db/client";
+
+const client = new PrismaClient();
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen text-5xl font-bold">
