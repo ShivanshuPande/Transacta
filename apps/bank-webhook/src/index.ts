46ec5e0b5cce1e27e031e91198231893b7cefbaa
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-08-03 21:56:12 +0530

    issues

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -7,12 +7,33 @@ app.use(express.json());
 const port = 3000;
 
 app.post("/webhookhdfc" , async (req ,res)=>{
-    const paymentInformation = {
+    const paymentInformation :{
+        token :string ,
+        userId : string,
+        amount : string
+    }={
         token : req.body.token,
         userId : req.body.user_identifier,
         amount : req.body.amount
-    }
+    };
     
+    
+    try{
+        await db.$transaction([
+            db.balance.updateMany({
+                where:{
+                    userId : Number(paymentInformation.userId)
+                },{
+                    data :{
+                        amount : {
+                            
+                        }
+                    }
+                }
+            })
+        ])
+    }
+
 })
 
 // write the code for transaction
